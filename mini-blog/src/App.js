4de1dc66199c138a311382bcc2b8152067fa6306
GabrielaSinastre <gabrielamaria.gaby@hotmail.com>
2024-06-23 14:28:41 -0300

    feat: footer, navbar and global styles

--- mini-blog/src/App.js
+++ mini-blog/src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="container">
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
         </Routes>
       </div>
+      <Footer />
     </BrowserRouter>
   );
 }
